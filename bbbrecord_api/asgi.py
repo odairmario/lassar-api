e822f01645a08b47ed5c1596e7fe00861325af83
Odair M <odairmario45@gmail.com>
2021-10-04 06:30:21 -0300

    feat(Core): add main app

---
+++ bbbrecord_api/asgi.py
@@ -0,0 +1,28 @@
+"""
+ASGI config for bbbrecord_api project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+import record.routing
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bbbrecord_api.settings')
+django_asgi_app = get_asgi_application()
+
+
+application = ProtocolTypeRouter({
+    "http": django_asgi_app,
+     "websocket": AuthMiddlewareStack(
+        URLRouter(
+            record.routing.websocket_urlpatterns
+        )
+    ),
+    })
